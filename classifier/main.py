b785fc3e83b455dad88482bfbda9161bbb334e46
Martín Vásquez <pedromvasquezb@gmail.com>
2017-04-18 13:16:52 -0500

    Add 'root' parameter to 'DataLoader.__init__'

--- classifier/main.py
+++ classifier/main.py
@@ -8,34 +8,34 @@ class Main(object):
     def __init__(self, career):
         super(Main, self).__init__()
         self.career = career
-        self.io = DataLoader()
+        self.io = DataLoader(root='..')
 
     def run(self, data_file):
         # (labelledData,
         #  dataset,
         #  dictionary,
-        #  categories) = self.io.getDataForCareer(self.career)
-        data = self.io.getDataForCareer(self.career)
-        import json
-        with open('data.json', 'w') as f:
-            json.dump(data, f, indent=2, ensure_ascii=False)
+        #  categories) = self.io.get_data_for_career(self.career)
+        data = self.io.get_data_for_career(self.career)
+        # import json
+        # with open('data.json', 'w') as f:
+        #     json.dump(data, f, indent=2, ensure_ascii=False)
         return ''
 
 
 def main(args=None):
-    from sklearn.datasets import fetch_20newsgroups
-    from sklearn.feature_extraction.text import TfidfVectorizer
+    # from sklearn.datasets import fetch_20newsgroups
+    # from sklearn.feature_extraction.text import TfidfVectorizer
 
-    categories = ['alt.atheism', 'soc.religion.christian',
-                  'comp.graphics', 'sci.med']
-    twenty_train = fetch_20newsgroups(subset='train', categories=categories,
-                                      shuffle=True, random_state=42)
+    # categories = ['alt.atheism', 'soc.religion.christian',
+    #               'comp.graphics', 'sci.med']
+    # twenty_train = fetch_20newsgroups(subset='train', categories=categories,
+    #                                   shuffle=True, random_state=42)
 
-    count_vec = TfidfVectorizer()
-    X_train_tfidf = count_vec.fit_transform(twenty_train.data)
-    print(X_train_counts)
+    # count_vec = TfidfVectorizer()
+    # X_train_tfidf = count_vec.fit_transform(twenty_train.data)
+    # print(X_train_counts)
 
-    return
+    # return
     if args is None:
         args = sys.argv
     if len(args) < 3:
